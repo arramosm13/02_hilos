26fab1dbc0a24f5dcfb0901b3defba854fd62eb9
alejandra <arramosm@unjbg.edu.pe>
2026-09-23 10:12:29 -0500

    Se reemplazó el archivo anterior para medir la variación del rendimiento al aumentar la carga de hilos.

--- hilos.c
+++ hilos.c
@@ -1,29 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <time.h>
 
 pthread_mutex_t mutex;
 
 void* hilo_funcion(void* arg) {
-    pthread_mutex_lock(&mutex);   // Bloquea el mutex
+    pthread_mutex_lock(&mutex);
     printf("Hola desde el hilo %ld\n", (long)arg);
-    pthread_mutex_unlock(&mutex); // Desbloquea el mutex
+    pthread_mutex_unlock(&mutex);
     pthread_exit(NULL);
 }
 
-int main() {
-    pthread_t hilo1, hilo2;
+void ejecutar_con_hilos(int num_hilos) {
+    pthread_t hilos[num_hilos];
 
     pthread_mutex_init(&mutex, NULL);
 
-    pthread_create(&hilo1, NULL, hilo_funcion, (void*)1);
-    pthread_create(&hilo2, NULL, hilo_funcion, (void*)2);
+    clock_t start = clock(); // Inicio del temporizador
 
-    pthread_join(hilo1, NULL);
-    pthread_join(hilo2, NULL);
+    for (long i = 0; i < num_hilos; i++) {
+        pthread_create(&hilos[i], NULL, hilo_funcion, (void*)(i + 1));
+    }
+
+    for (int i = 0; i < num_hilos; i++) {
+        pthread_join(hilos[i], NULL);
+    }
+
+    clock_t end = clock(); // Fin del temporizador
 
     pthread_mutex_destroy(&mutex);
 
-    printf("Finalizo la ejecucion del programa principal\n");
+    double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Tiempo total de ejecución con %d hilos: %f segundos\n\n", num_hilos, cpu_time_used);
+}
+
+int main() {
+    printf("Ejecutando con 1 hilo...\n");
+    ejecutar_con_hilos(1);
+
+    printf("Ejecutando con 5 hilos...\n");
+    ejecutar_con_hilos(5);
+
+    printf("Ejecutando con 10 hilos...\n");
+    ejecutar_con_hilos(10);
+
+    printf("Finalizó la ejecución del programa principal\n");
     return 0;
 }
